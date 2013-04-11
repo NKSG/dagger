1c9da83f186bca358dae2faf640f1075a14a0827
Hrvoje Zeba <zeba.hrvoje@gmail.com>
2013-04-12 01:17:45 +0200

    * some basic stuff around channels, work in progress

---
+++ src/channel.h
@@ -0,0 +1,85 @@
+#pragma once
+
+
+#include "types.h"
+
+#include <algorithm>
+#include <cassert>
+
+
+namespace dagger {
+
+
+class channel
+{
+public:
+
+public:
+    const int max_value = 100000000;
+    
+public:
+    channel(uint16_t height, uint16_t width)
+    {
+        create_channel(height, width);
+    }
+
+public:
+    int16_t height() const
+    {
+        return m_height;
+    }
+    
+    int16_t width() const
+    {
+        return m_width;
+    }
+    
+    const data_t data() const
+    {
+        return m_data;
+    }
+
+    data_t data()
+    {
+        return m_data;
+    }
+
+public:
+    channel(const channel& other)
+    {
+        *this = other;
+    }
+
+    channel& operator=(const channel& other)
+    {
+        create_channel(other.m_height, other.m_width);
+        std::copy_n(other.m_data.get(), m_image_size, m_data.get());
+        
+        return *this;
+    }
+
+private:
+    void create_channel(int16_t height, int16_t width)
+    {
+        assert(height > 0);
+        assert(width > 0);
+        
+        m_height = height;
+        m_width = width;
+        
+        m_image_size = height * width;
+        
+        m_data.reset(new int32_t[m_image_size]);
+        std::fill_n(m_data.get(), m_image_size, 0);
+    }
+
+private:
+    data_t  m_data;
+    
+    int16_t m_height;
+    int16_t m_width;
+    
+    int32_t m_image_size;    
+};
+
+}
---
+++ src/types.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+#include <memory>
+#include <vector>
+#include <cstdint>
+
+
+namespace dagger {
+
+
+typedef std::shared_ptr<int32_t> data_t;
+typedef std::vector<bool>        bit_mask_t;
+
+}
